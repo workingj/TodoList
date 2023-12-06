f43e90369ce35b0d96f1398820793fafa3591ea9
Tonio <tonio.suessdorf@gmail.com>
2023-12-06 15:55:59 +0100

    Add popup example html and js files

---
+++ src/openPopup.js
@@ -0,0 +1,11 @@
+// Function to open the popup
+function openPopup() {
+  document.getElementById("popup").style.display = "block";
+  document.getElementById("overlay").style.display = "block";
+}
+
+// Function to close the popup
+function closePopup() {
+  document.getElementById("popup").style.display = "none";
+  document.getElementById("overlay").style.display = "none";
+}
