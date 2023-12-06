4c7069558f2b636989d751029774dab95a257545
Tonio <tonio.suessdorf@gmail.com>
2023-12-06 18:16:12 +0100

    Change openPopup.js to accomodate different popups

--- src/openPopup.js
+++ src/openPopup.js
@@ -1,11 +1,24 @@
-// Function to open the popup
-function openPopup() {
-  document.getElementById("popup").style.display = "block";
-  document.getElementById("overlay").style.display = "block";
+const createButton = document.getElementById("create-button");
+console.log(createButton);
+
+const createPopup = document.getElementById("popup-create");
+console.log(createPopup);
+
+const overlay = document.getElementById("overlay");
+
+let currentPopup = null;
+
+// Function to open a popup
+function openPopup(popup) {
+  currentPopup = popup;
+  popup.style.display = "block";
+  overlay.style.display = "block";
 }
 
 // Function to close the popup
 function closePopup() {
-  document.getElementById("popup").style.display = "none";
-  document.getElementById("overlay").style.display = "none";
+  currentPopup.style.display = "none";
+  overlay.style.display = "none";
 }
+
+createButton.addEventListener("click", (event) => openPopup(createPopup));
