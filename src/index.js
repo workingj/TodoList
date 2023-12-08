a6a8740452145eea4a74fb12182cbb67d9d3f7d1
workingj <workingj@pm.me>
2023-12-08 15:05:10 +0100

    Added easter Egg

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const popupCreate = document.querySelector("#popupCreate");
 const popupDelete = document.querySelector("#popupDelete");
+const popupEasterEgg = document.querySelector("#popupEasterEgg");
 const popupEdit = document.querySelector("#popupEdit");
 const overlay = document.getElementById("overlay");
 let currentEvent = "";
@@ -258,6 +259,7 @@ function openPopupCreate(event) {
   overlay.style.display = "block";
   document.getElementById("todo-text").focus();
 }
+
 function openPopupEdit(event) {
   const textinput = document.querySelector("#edit-text");
   const deadlineinput = document.querySelector("#deadline-edit");
@@ -277,6 +279,11 @@ function openPopupDelete(event) {
   overlay.style.display = "block";
 }
 
+function easterEgg() {
+  let text = document.getElementById("eggField");
+  popupEasterEgg.style.display = "block";
+}
+
 function maybeDelete() {
   Math.round(Math.random()) === 1 ? listItemDelete() : null;
   closePopup();
@@ -286,6 +293,7 @@ function closePopup() {
   popupCreate.style.display = "none";
   popupEdit.style.display = "none";
   popupDelete.style.display = "none";
+  popupEasterEgg.style.display = "none";
   overlay.style.display = "none";
 }
 
